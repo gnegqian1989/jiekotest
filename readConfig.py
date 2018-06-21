3ce68941539bddff295db42d349306874c160020
gengqian1989 <1585620775@qq.com>
2018-06-21 17:52:54 +0800

    添加读配置文件和目录说明

--- readConfig.py
+++ readConfig.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python
+#coading:utf-8
+
 import os
 import codecs
 import configparser
 
-pro
+proDir = os.path.split(os.path.realpath(__file__))[0]
+configPath = os.path.join(proDir,"config.ini")
+
+class
